f4c822c8d23845faf3f6f48526daf024a61214ca
Yamini-Durga <101850697+Yamini-Durga@users.noreply.github.com>
2022-06-28 19:51:42 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  FIREBASE_WEB_API_KEY: 'AIzaSyC0pZ38Ts_xmWtPIxy5FAf47c_4LAI8X88',
+  FIREBASE_WEB_API_KEY: 'Key_Api',
   ADMIN_U: 'admin@test.com',
   ADMIN_P: 'admin123'
 };
